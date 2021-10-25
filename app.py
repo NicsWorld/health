a7674522b90ee93e32f2dfeabcf2a7be9bd4f6d1
nic <nicwoods63@gmail.com>
2021-10-24 19:46:12 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from ping3 import ping
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
 @app.route("/")
 def home():
     return render_template('index.html')
